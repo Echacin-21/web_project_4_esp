34244dacaf6691cc3d76e36f41503f617c584a20
eduardo chacin <ech2194@gmail.com>
2023-09-14 14:47:01 -0300

    Avances de proyecto 6

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,26 @@
-let container = document.querySelector(".container");
-let page = document.querySelector(".page");
-let btnClose = page.querySelector(".page__button-close");
-let button = container.querySelector(".profile__button-edit");
-let form = page.querySelector(".page__form-profile");
-let addPlace = page.querySelector(".page__form-card");
-let element = container.querySelector(".elements");
-let cards = document.querySelectorAll(".card");
-let btnTrash = element.querySelectorAll(".card__trash");
-let btnAdd = container.querySelector(".profile__button-add");
-let btnHeart = element.querySelectorAll(".card__heart");
-let profile = container.querySelector(".profile__info");
-let btnRefresh = document.querySelector(".page__form-profile-button");
+const container = document.querySelector(".container");
+const elements = container.querySelector(".elements");
+const page = document.querySelector(".page");
+const btnClose = page.querySelector(".page__button-close");
+const button = container.querySelector(".profile__button-edit");
+const addCard = document.querySelector(".page__form-card-button");
+const form = page.querySelector(".page__form-profile");
+const addPlace = page.querySelector(".page__form-card");
+const element = container.querySelector(".elements");
+const cards = document.querySelectorAll(".card");
+const btnTrash = document.querySelectorAll(".card__trash");
+const btnAdd = container.querySelector(".profile__button-add");
+const btnHeart = element.querySelectorAll(".card__heart");
+const profile = container.querySelector(".profile__info");
+const btnRefresh = document.querySelector(".page__form-profile-button");
+let btnImg = document.querySelectorAll(".card__image");
+const imgContent = document.querySelector(".page__img-content");
+const btnCloseImg = document.querySelector(".page__button");
 
 form.classList.add("display-none");
 addPlace.classList.add("display-none");
 btnClose.classList.add("display-none");
+imgContent.classList.add("display-none");
 
 function onForm() {
   form.classList.remove("display-none");
@@ -29,9 +35,11 @@ function offForm() {
   addPlace.classList.remove("display-flex");
   btnClose.classList.remove("display-block");
   container.classList.remove("container_filter");
+  imgContent.classList.remove("display-flex");
   form.classList.add("display-none");
   addPlace.classList.add("display-none");
   btnClose.classList.add("display-none");
+  imgContent.classList.add("display-none");
 }
 
 function newplace() {
@@ -44,10 +52,12 @@ function newplace() {
 
 function editprofile(evt) {
   evt.preventDefault();
-  let name = document.querySelector(".profile__name");
-  let description = document.querySelector(".profile__description");
-  let profileocupation = document.querySelector(".page__form-profile-ocupation");
-  let editname = document.querySelector(".page__form-profile-name");
+  const name = document.querySelector(".profile__name");
+  const description = document.querySelector(".profile__description");
+  const profileocupation = document.querySelector(
+    ".page__form-profile-ocupation"
+  );
+  const editname = document.querySelector(".page__form-profile-name");
   name.textContent = editname.value;
   description.textContent = profileocupation.value;
   editname.value = "Jacques Cousteau";
@@ -79,7 +89,89 @@ btnHeart.forEach((boton, index) => {
   });
 });
 
+function newCard(evt) {
+  evt.preventDefault();
+  const imgTittle = document.querySelector(".page__form-card-name");
+  const imgUrl = document.querySelector(".page__form-card-ocupation");
+
+  const cardTemplate = document.querySelector("#template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = imgUrl.value;
+  cardElement.querySelector(".card__text").textContent = imgTittle.value;
+  cardElement
+    .querySelector(".card__trash")
+    .addEventListener("click", function (evt) {
+      const cardContainer = cardElement.parentElement;
+      cardContainer.removeChild(cardElement);
+    });
+  let btnActive = false;
+  cardElement
+    .querySelector(".card__heart")
+    .addEventListener("click", function (evt) {
+      if (!btnActive) {
+        evt.target.setAttribute(
+          "style",
+          "background-image: url(./image/Vectorheart-black.svg)"
+        );
+
+        btnActive = true;
+      } else {
+        evt.target.removeAttribute("style");
+        btnActive = false;
+      }
+    });
+
+  cardElement.querySelector(".card__image").addEventListener("click", function (evt) {
+    container.classList.add("container_filter");
+    console.log("pulsaste la imagen");
+    const cardImage = document.querySelectorAll(".card__image");
+    const imgContent = document.querySelector(".page__img-content");
+    const imgNameElement = document.querySelectorAll(".card__text");
+    const srcImg = cardImage.getAttribute("src");
+    const img = document.querySelector(".page__img");
+    img.src = srcImg;
+
+      // const imgName = imgNameElement[index].textContent;
+      // const nameElement = document.querySelector(".page__img-name");
+      // nameElement.textContent = imgName;
+
+    imgContent.classList.add("display-flex");
+  });
+
+  elements.append(cardElement);
+
+  imgTittle.value = "";
+  imgUrl.value = "";
+
+  offForm();
+}
+
+// Abrir popup de imagen.
+btnImg.forEach((imagen, index) => {
+  imagen.addEventListener("click", () => {
+    container.classList.add("container_filter");
+    console.log("pulsaste la imagen");
+    const imgContent = document.querySelector(".page__img-content");
+    const imgNameElement = document.querySelectorAll(".card__text");
+    const srcImg = btnImg[index].getAttribute("src");
+    const img = document.querySelector(".page__img");
+    img.src = srcImg;
+
+    const imgName = imgNameElement[index].textContent;
+    const nameElement = document.querySelector(".page__img-name");
+    nameElement.textContent = imgName;
+
+    imgContent.classList.add("display-flex");
+  });
+});
+
 button.addEventListener("click", onForm);
 btnAdd.addEventListener("click", newplace);
 btnClose.addEventListener("click", offForm);
 btnRefresh.addEventListener("click", editprofile);
+addCard.addEventListener("click", newCard);
+btnCloseImg.addEventListener("click", offForm);
+
+//   openImage(btnImg.src);
+// });

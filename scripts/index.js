db4b5866f9619832dfb131a78ebd392b26543e71
eduardo chacin <ech2194@gmail.com>
2023-10-14 16:51:17 -0300

    Inicio de proyecto 8

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,7 @@
-const initialCards = [
-  {
-    name: "Valle de Yosemite",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
-  },
-  {
-    name: "Lago Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
-  },
-  {
-    name: "Montañas Calvas",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
-  },
-  {
-    name: "Parque Nacional de la Vanoise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
-  },
-];
+import { initialCards } from "./array.js";
+
+console.log(initialCards);
+
 const container = document.querySelector(".container");
 // const elements = container.querySelector(".elements");
 const page = document.querySelector(".page");
@@ -142,16 +120,15 @@ function offForm() {
   addPlace.style.animation = "popupOff .3s linear";
   imgContent.style.animation = "popupOff .3s linear";
 
-  setTimeout(function(){
+  setTimeout(function () {
     formProfile.classList.remove("display-flex");
-  formProfile.classList.add("display-none");
-  addPlace.classList.remove("display-flex");
-  container.classList.remove("container_filter");
-  imgContent.classList.remove("display-flex");
-  addPlace.classList.add("display-none");
-  imgContent.classList.add("display-none");
+    formProfile.classList.add("display-none");
+    addPlace.classList.remove("display-flex");
+    container.classList.remove("container_filter");
+    imgContent.classList.remove("display-flex");
+    addPlace.classList.add("display-none");
+    imgContent.classList.add("display-none");
   }, 250);
-  
 
   clickOffPopupOff();
 }
